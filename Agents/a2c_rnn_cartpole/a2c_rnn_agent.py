c4c67f5c964981a31219e1fe6cf1b235d59644b4
Mingwei Ma <mingwei.ma@queens.ox.ac.uk>
2019-11-20 22:20:16 +0000

    Add Args

--- Agents/a2c_rnn_cartpole/a2c_rnn_agent.py
+++ Agents/a2c_rnn_cartpole/a2c_rnn_agent.py
@@ -8,19 +8,31 @@ from keras import optimizers
 
 
 class ACRnnAgent(object):
-    def __init__(self, ob_dim, ac_dim):
+    def __init__(self,
+                 ob_dim,
+                 ac_dim,
+                 action_space=[0, 1],
+                 learning_rate=1e-3,
+                 gamma=0.99,
+                 max_path_length=600,
+                 batch_size=1000,
+                 num_grad_steps_per_target_update=10,
+                 num_target_updates=10,
+                 lookback=5,
+                 initial_exploration_eps=10
+                 ):
         self.ob_dim = ob_dim
         self.ac_dim = ac_dim
-        self.action_space = [0, 1]
-        self.learning_rate = 5e-3
-        self.gamma = 0.99
-        self.max_path_length = 600
-        self.batch_size = 1000
+        self.action_space = action_space
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.max_path_length = max_path_length
+        self.batch_size = batch_size
         self.normalize_advantages = True
-        self.num_grad_steps_per_target_update = 10
-        self.num_target_updates = 10
-        self.lookback = 5
-        self.initial_exploration_eps = 10
+        self.num_grad_steps_per_target_update = num_grad_steps_per_target_update
+        self.num_target_updates = num_target_updates
+        self.lookback = lookback
+        self.initial_exploration_eps = initial_exploration_eps
 
         self.actor_model = self.initialize_actor_model()
         self.__build_train_fn()
@@ -87,8 +99,8 @@ class ACRnnAgent(object):
             ob, rew, done, _ = env.step(ac)
             next_obs_seq = np.concatenate([next_obs_seq_candidate, np.asarray([ob])])
             next_obs_seq = next_obs_seq.reshape((self.lookback, self.ob_dim))
-            # if render and self.num_sampled_trajectories % animate_eps_frequency == 0:
-            #     env.render()
+            if render and self.num_sampled_trajectories % animate_eps_frequency == 0:
+                env.render()
             acs.append(ac)
             next_obs.append(ob)
             next_obs_seq_s.append(next_obs_seq)
@@ -116,8 +128,6 @@ class ACRnnAgent(object):
         return ac
 
     def estimate_advantage(self, ob_seq, next_ob_seq, re_n, terminal_n):
-        ob_seq = ob_seq.reshape((-1, self.lookback, self.ob_dim))
-        next_ob_seq = next_ob_seq.reshape((-1, self.lookback, self.ob_dim))
         v_n = self.critic_model.predict(ob_seq).flatten()
         next_v_n = self.critic_model.predict(next_ob_seq).flatten()
         adv_n = re_n + (1 - terminal_n) * self.gamma * next_v_n - v_n
--- Agents/a2c_rnn_cartpole/a2c_rnn_train.py
+++ Agents/a2c_rnn_cartpole/a2c_rnn_train.py
@@ -8,26 +8,27 @@ discrete = isinstance(env.action_space, gym.spaces.Discrete)
 ob_dim = env.observation_space.shape[0]
 ac_dim = env.action_space.n if discrete else env.action_space.shape[0]
 
-agent = ACRnnAgent(ob_dim, ac_dim)
-
-
-# # build computation graph
-# agent.build_computation_graph()
-#
-# # tensorflow: config, session, variable initialization
-# agent.init_tf_sess()
+agent = ACRnnAgent(ob_dim,
+                   ac_dim,
+                   batch_size=2000)
 
 avg_rews = []
-n_iter = 100
+n_iter = 80
 for itr in range(n_iter):
     print("********** Iteration %i ************" % itr)
     ob_seq, next_ob_seq, ac_na, re_n, terminal_n, avg_rew = \
-        agent.sample_trajectories(env, render=True, animate_eps_frequency=10)
+        agent.sample_trajectories(env,
+                                  render=False,
+                                  animate_eps_frequency=10,
+                                  )
     avg_rews.append(avg_rew)
     print(avg_rew)
 
     agent.update_critic(ob_seq, next_ob_seq, re_n, terminal_n)
     adv_n = agent.estimate_advantage(ob_seq, next_ob_seq, re_n, terminal_n)
     agent.update_actor(ob_seq, ac_na, adv_n)
+    # if itr == 70:
+        # agent.actor_model.save(f'dra2c_cartpole_actor_{int(itr)}_itr.h5')
+        # agent.critic_model.save(f'dra2c_cartpole_critic_{int(itr)}_itr.h5')
 
 plot_with_avg_std(avg_rews, 1, xlabel=f'Number of Episodes in {1}')
