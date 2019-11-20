006fc908dc323a5d6c7c19f3201fb9b5d8e39641
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2019-11-20 21:47:03 +0000

    Add A2C RNN

--- Agents/a2c_rnn_cartpole/a2c_rnn_train.py
+++ Agents/a2c_rnn_cartpole/a2c_rnn_train.py
@@ -1,6 +1,6 @@
 import gym
 import numpy as np
-from Agents.a2c_cartpole.ac_agent import ACAgent
+from Agents.a2c_rnn_cartpole.a2c_rnn_agent import ACRnnAgent
 from tools.plot_tool import plot_with_avg_std
 env = gym.make('CartPole-v1')
 
@@ -8,7 +8,7 @@ discrete = isinstance(env.action_space, gym.spaces.Discrete)
 ob_dim = env.observation_space.shape[0]
 ac_dim = env.action_space.n if discrete else env.action_space.shape[0]
 
-agent = ACAgent(ob_dim, ac_dim)
+agent = ACRnnAgent(ob_dim, ac_dim)
 
 
 # # build computation graph
@@ -19,22 +19,15 @@ agent = ACAgent(ob_dim, ac_dim)
 
 avg_rews = []
 n_iter = 100
-total_timesteps = 0
 for itr in range(n_iter):
     print("********** Iteration %i ************" % itr)
-    paths, timesteps_this_batch, avg_rew = agent.sample_trajectories(env, render=True, animate_eps_frequency=10)
+    ob_seq, next_ob_seq, ac_na, re_n, terminal_n, avg_rew = \
+        agent.sample_trajectories(env, render=True, animate_eps_frequency=10)
     avg_rews.append(avg_rew)
     print(avg_rew)
-    total_timesteps += timesteps_this_batch
 
-    ob_no = np.concatenate([path["observation"] for path in paths])
-    ac_na = np.concatenate([path["action"] for path in paths])
-    re_n = np.concatenate([path["reward"] for path in paths])
-    next_ob_no = np.concatenate([path["next_observation"] for path in paths])
-    terminal_n = np.concatenate([path["terminal"] for path in paths])
-
-    agent.update_critic(ob_no, next_ob_no, re_n, terminal_n)
-    adv_n = agent.estimate_advantage(ob_no, next_ob_no, re_n, terminal_n)
-    agent.update_actor(ob_no, ac_na, adv_n)
+    agent.update_critic(ob_seq, next_ob_seq, re_n, terminal_n)
+    adv_n = agent.estimate_advantage(ob_seq, next_ob_seq, re_n, terminal_n)
+    agent.update_actor(ob_seq, ac_na, adv_n)
 
 plot_with_avg_std(avg_rews, 1, xlabel=f'Number of Episodes in {1}')
