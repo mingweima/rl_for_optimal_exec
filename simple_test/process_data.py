57a70c53ac9dff693e94273f5c15152656051ec1
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-01-24 22:49:58 +0000

    unknown modification

--- simple_test/process_data.py
+++ simple_test/process_data.py
@@ -21,14 +21,17 @@ def animate():
 t = threading.Thread(target=animate)
 t.start()
 
-train_paths = [f'/nfs/home/mingweim/lob/hsbc/L2_HSBA.L_{month}.csv.gz'
-                for month in
-               ['2018-01-01_2018-01-31', '2018-02-01_2018-02-28', '2018-03-01_2018-03-31', '2018-04-01_2018-04-30',
-                '2018-05-01_2018-05-31', '2018-06-01_2018-06-30', '2018-07-01_2018-07-31', '2018-08-01_2018-08-31']]
+# train_paths = [f'/nfs/home/mingweim/lob/hsbc/L2_HSBA.L_{month}.csv.gz'
+#                 for month in
+#                ['2018-01-01_2018-01-31', '2018-02-01_2018-02-28', '2018-03-01_2018-03-31', '2018-04-01_2018-04-30',
+#                 '2018-05-01_2018-05-31', '2018-06-01_2018-06-30', '2018-07-01_2018-07-31', '2018-08-01_2018-08-31']]
 
-test_paths =  [f'/nfs/home/mingweim/lob/hsbc/L2_HSBA.L_{month}.csv.gz'
-                for month in
-               ['2018-09-01_2018-09-30', '2018-10-01_2018-10-31', '2018-11-01_2018-11-30', '2018-12-01_2018-12-31']]
+train_paths = [f'/nfs/home/mingweim/lob/hsbc/L2_HSBA.L_2018-01-01_2018-01-31.csv.gz']
+test_paths = [f'/nfs/home/mingweim/lob/hsbc/L2_HSBA.L_2018-01-01_2018-01-31.csv.gz']
+
+# test_paths =  [f'/nfs/home/mingweim/lob/hsbc/L2_HSBA.L_{month}.csv.gz'
+#                 for month in
+#                ['2018-09-01_2018-09-30', '2018-10-01_2018-10-31', '2018-11-01_2018-11-30', '2018-12-01_2018-12-31']]
 
 
 train_raw_data_list = [pd.read_csv(path_name, compression='gzip', error_bad_lines=False) for path_name in train_paths]
