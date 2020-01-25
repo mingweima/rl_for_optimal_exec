3da7d5caf6f3edaa0b520a83030a989115aadcf2
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-01-25 00:39:34 +0000

    Update process_data.py

--- simple_test/process_data.py
+++ simple_test/process_data.py
@@ -91,18 +91,40 @@ pickle.dump(train_data, df_train)
 df_train.close()
 
 for month in test_months:
-    bar.set_description('Processing Test Data -- {}'.format(month))
+    bar = tqdm(range(6))
+    bar.set_description('Reading Data -- {}'.format(month))
     path_name = '/nfs/home/mingweim/lob/hsbc/L2_HSBA.L_{}.csv.gz'.format(month)
+
     raw_data = pd.read_csv(path_name, compression='gzip', error_bad_lines=False)
+
+    bar.update(1)
+    bar.set_description('Dropping Columns -- {}'.format(month))
+
     data = raw_data.drop(['#RIC', 'Domain', 'GMT Offset', 'Type', 'L1-BuyNo', 'L1-SellNo', 'L2-BuyNo', 'L2-SellNo',
                           'L3-BuyNo', 'L3-SellNo', 'L4-BuyNo', 'L4-SellNo', 'L5-BuyNo', 'L5-SellNo',
                           'L6-BuyNo', 'L6-SellNo', 'L7-BuyNo', 'L7-SellNo', 'L8-BuyNo', 'L8-SellNo',
                           'L9-BuyNo', 'L9-SellNo', 'L10-BuyNo', 'L10-SellNo'], axis=1)
+
+    bar.update(1)
+    bar.set_description('Rounding to Time Integer -- {}'.format(month))
+
     data['Date-Time'] = pd.to_datetime(data['Date-Time'],
                                        format='%Y-%m-%dT%H:%M:%S.%fZ').dt.round('{}s'.format(600))
+
+    bar.update(1)
+    bar.set_description('Grouping By -- {}'.format(month))
+
     data = data.groupby(['Date-Time']).first().reset_index()
+
+    bar.update(1)
+    bar.set_description('Deleting Weekends -- {}'.format(month))
+
     data['Day'] = data['Date-Time'].dt.dayofweek
     data = data.drop(data.loc[(data['Day'] == 5) | (data['Day'] == 6)].index)
+
+    bar.update(1)
+    bar.set_description('Deleting Auction Periods -- {}'.format(month))
+
     data['Hour'] = data['Date-Time'].dt.hour
     data['Minute'] = data['Date-Time'].dt.minute
     data = data.drop(
@@ -110,6 +132,10 @@ for month in test_months:
     data = data.drop(['Hour', 'Minute', 'Day'], axis=1)
     data = data.iloc[1:]
     test_data_list.append(data)
+
+    bar.update(1)
+    bar.set_description('Finished Processing Data -- {}'.format(month))
+    bar.close()
 
 test_data = pd.concat(test_data_list, ignore_index=True)
 date = pd.to_datetime(test_data['Date-Time'].dt.strftime('%Y/%m/%d'))
