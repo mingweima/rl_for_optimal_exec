0d056823b1c494af52b3c89804aaa69f95648c37
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-01-26 18:39:18 +0000

    Update dddqn_train.py

--- agents/dddqn/dddqn_train.py
+++ agents/dddqn/dddqn_train.py
@@ -198,10 +198,11 @@ def dddqn_train(hyperparameters, ac_dict, ob_dict, train_months, test_months):
     ### MODIFY THIS TO FALSE IF YOU JUST WANT TO SEE THE TRAINED AGENT
     training = True
 
-    config = tf.ConfigProto(
-        device_count={"CPU": 40},
-        inter_op_parallelism_threads=8,
-        intra_op_parallelism_threads=8)
+    # config = tf.ConfigProto(
+    #     device_count={"CPU": 40},
+    #     inter_op_parallelism_threads=40,
+    #     intra_op_parallelism_threads=40)
+    config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     set_session(tf.Session(config=config))
 
