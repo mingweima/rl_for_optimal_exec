7800020f3ac266b35c704f81f2d72fbd4d9c01b0
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-01-30 06:15:44 +0000

    r

--- agents/dddqn/dddqn_train.py
+++ agents/dddqn/dddqn_train.py
@@ -479,6 +479,25 @@ def dddqn_train(hyperparameters, ac_dict, ob_dict, train_months, test_months):
         if loop_indx % target_network_update == 0:
             update_target = update_target_graph()
             sess.run(update_target)
+            bar = tqdm(range(num_of_test_days * 2), leave=False)
+            bar.set_description("Testing Results")
+            reward_list = []
+            for month in test_date.keys():
+                for day in test_date[month]:
+                    for session in ['morning', 'afternoon']:
+                        bar.update(1)
+                        check_reward = te_performance(month, day, session)
+                        reward_list.append(check_reward)
+            bar.close()
+
+            avg_re = np.average(reward_list)
+            test_f = open(dirpath + '/test.txt', 'a+')
+            print('Loop {}, Test Average Reward: '.format(loop_indx), round(avg_re, 3), '\n', file=test_f)
+            test_f.close()
+            print('Loop {}, Test Average Reward: '.format(loop_indx), round(avg_re, 3))
+            test_avg_reward.append(avg_re)
+            avg_re_per_loop.append(np.mean(total_reward_list))
+            loss_per_loop.append(np.average(losses))
 
         print(f'{datetime.datetime.now()} '
               f'Loop = {loop}, '
@@ -494,25 +513,7 @@ def dddqn_train(hyperparameters, ac_dict, ob_dict, train_months, test_months):
               f'Explore P = {explore_probability}\n', file=train_f)
         train_f.close()
 
-        bar = tqdm(range(num_of_test_days * 2), leave=False)
-        bar.set_description("Testing Results")
-        reward_list = []
-        for month in test_date.keys():
-            for day in test_date[month]:
-                for session in ['morning', 'afternoon']:
-                    bar.update(1)
-                    check_reward = te_performance(month, day, session)
-                    reward_list.append(check_reward)
-        bar.close()
 
-        avg_re = np.average(reward_list)
-        test_f = open(dirpath + '/test.txt', 'a+')
-        print('Loop {}, Test Average Reward: '.format(loop_indx), round(avg_re, 3), '\n', file=test_f)
-        test_f.close()
-        print('Loop {}, Test Average Reward: '.format(loop_indx), round(avg_re, 3))
-        test_avg_reward.append(avg_re)
-        avg_re_per_loop.append(np.mean(total_reward_list))
-        loss_per_loop.append(np.average(losses))
 
         if loop_indx % 50 == 0:
             fig1 = plt.figure()
