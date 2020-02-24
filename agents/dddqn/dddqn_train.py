52439e124b887685d6f8b62c3adc61c58be25a13
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-02-24 23:14:00 +0000

    Update dddqn_train.py

--- agents/dddqn/dddqn_train.py
+++ agents/dddqn/dddqn_train.py
@@ -506,15 +506,16 @@ def dddqn_train(hyperparameters, ac_dict, ob_dict, train_months, test_months):
                                 total_reward_list.append(total_reward)
                             else:
                                 memory.add((state, action, reward, next_state, done))
-                                print("Shape: ", state.shape)
                                 state = next_state
 
                             ### Training Network
                             # Obtain random mini-batch from memory
                             batch = memory.sample(batch_size)
+                            print(batch)
+                            print("Batch: ", batch.shape)
                             states_mb = np.array([each[0] for each in batch])
-                            print(states_mb)
-                            print(states_mb.shape)
+                            # print(states_mb)
+                            # print(states_mb.shape)
                             # states_mb.reshape(np.concatenate(([batch_size], np.array(state_size))))
                             actions_mb = np.array([each[1] for each in batch])
                             rewards_mb = np.array([each[2] for each in batch])
