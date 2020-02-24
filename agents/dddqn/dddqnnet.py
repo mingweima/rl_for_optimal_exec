601cb6d95dfaf52f3a828caecb9e38b52be648a2
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-02-24 21:29:19 +0000

    Update dddqnnet.py

--- agents/dddqn/dddqnnet.py
+++ agents/dddqn/dddqnnet.py
@@ -60,12 +60,12 @@ class DDDQNNet:
 
             ## Here we separate into two streams
             # The one that calculate V(s)
-            self.value_fc = tf.keras.layers.CuDNNLSTM(32)(self.conv_first1)
+            self.value_fc = tf.keras.layers.LSTM(32)(self.conv_first1)
             #             self.value_fc = tf.keras.layers.LeakyReLU(alpha=0.01)(self.value_fc)
             self.value = tf.keras.layers.Dense(1)(self.value_fc)
 
             #             The one that calculate A(s,a)
-            self.advantage_fc = tf.keras.layers.CuDNNLSTM(32)(self.conv_first1)
+            self.advantage_fc = tf.keras.layers.LSTM(32)(self.conv_first1)
             #             self.advantage_fc = tf.keras.layers.LeakyReLU(alpha=0.01)(self.advantage_fc)
             self.advantage = tf.keras.layers.Dense(self.action_size)(self.advantage_fc)
 
