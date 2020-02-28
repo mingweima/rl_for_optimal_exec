88e3e030b5d2dc9c62f08a0dd70ef6a44635d7d1
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-02-28 22:20:35 +0000

    Update trading_env.py

--- trading_environment/trading_env.py
+++ trading_environment/trading_env.py
@@ -169,8 +169,7 @@ class Simulator:
         self.observation_sequence.append(obs)
 
         state = np.array(self.observation_sequence[-self.look_back:])
-        state = list(state.reshape(state.shape + (1,)))
-        # state = self.observation_sequence[-self.look_back:]
+        # state = list(state.reshape(state.shape + (1,)))
         return state, reward, done, info
 
     def observation(self):
