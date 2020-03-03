8a82946893686aaf3517d02d4cfdbda0dbef6538
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-03-03 02:22:32 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dirpath = '/Users/gongqili/recordings/all/loop100_2020-03-02_10:40:10'
+AC_dirpath = '/Users/gongqili/recordings/all/loop100_2020-03-03_00:50:12'
 
 loop = 15
 
@@ -14,39 +15,55 @@ loop = 15
 # RDSa_AC_res = pickle.load(file6, encoding='iso-8859-1')
 # for i in range(len(ULVR_AC_res)):
 #     ULVR_AC_sum.append(np.sum(ULVR_AC_res[:i]))
-
+kappas = [0.2, 0.3, 0.4, 0.5]
 tickers = ['ULVR', 'RR', 'RDSa']
-for loop in np.arange(23, 24):
-    res = []
-    dones = []
-    for ticker in tickers:
-        file = open(dirpath + '/loop{}_{}_test_res.txt'.format(loop, ticker), 'rb')
-        re = pickle.load(file, encoding='iso-8859-1')
-        res.append(re)
-        file = open(dirpath + '/loop{}_{}_test_dones.txt'.format(loop, ticker), 'rb')
-        done = pickle.load(file, encoding='iso-8859-1')
-        dones.append(done)
+
+for ticker in tickers:
+    AC_res = {}
+    for kappa in kappas:
+        AC_res[kappa] = []
+
+    file = open(dirpath + '/loop{}_{}_test_res.txt'.format(loop, ticker), 'rb')
+    res = pickle.load(file, encoding='iso-8859-1')
+    file = open(dirpath + '/loop{}_{}_test_dones.txt'.format(loop, ticker), 'rb')
+    dones = pickle.load(file, encoding='iso-8859-1')
+    for kappa in kappas:
+        file = open(AC_dirpath + '/{}_ACtest_res_kappa{}.txt'.format(ticker, kappa), 'rb')
+        AC_res[kappa] = pickle.load(file, encoding='iso-8859-1')
 
     ep_res = []
-    for i in range(len(tickers)):
-        for indx in range(len(dones[i])):
-            if indx == 0:
-                ep_re = res[i][ : dones[i][indx] + 1]
-                while len(ep_re) < 24:
-                    ep_re.append(0)
-                ep_res.append(ep_re)
-            else:
-                ep_re = res[i][dones[i][indx - 1] + 1 : dones[i][indx]]
-                while len(ep_re) < 24:
-                    ep_re.append(0)
-                ep_res.append(ep_re)
+    for indx in range(len(dones)):
+        if indx == 0:
+            ep_re = res[ : dones[indx] + 1]
+            while len(ep_re) < 24:
+                ep_re.append(0)
+            ep_res += ep_re
+        else:
+            ep_re = res[dones[indx - 1] + 1 : dones[indx]]
+            while len(ep_re) < 24:
+                ep_re.append(0)
+            ep_res += ep_re
 
-    ep_res = list(np.array(ep_res).flatten())
     accumulated_res = []
     for i in range(len(ep_res)):
         accumulated_res.append(np.sum(ep_res[:i]))
-    plt.plot(accumulated_res)
-    plt.title('Accumulated Profit over Hothead Agent')
+
+    accumulated_AC_res = {}
+    for kappa in kappas:
+        accumulated_AC_res[kappa] = []
+        for i in range(len(AC_res[kappa])):
+            accumulated_AC_res[kappa].append(np.sum(AC_res[kappa][:i]))
+
+    plt.plot(accumulated_res, label='RL Model')
+    for kappa in kappas:
+        plt.plot(accumulated_AC_res[kappa], label='AC Model, k = {}'.format(kappa))
+    plt.title('Accumulated Profit over Hothead Agent, {}'.format(ticker))
     plt.xlabel('Trading Steps')
     plt.ylabel('Profit')
+    plt.legend()
     plt.show()
+
+    print('RL', np.average(ep_res), np.std(ep_res), np.average(ep_res)/np.std(ep_res))
+    for kappa in kappas:
+        print('AC kappa = {}'.format(kappa), np.average(AC_res[kappa]),
+              np.std(AC_res[kappa]), np.average(AC_res[kappa])/np.std(AC_res[kappa]))
