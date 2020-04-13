4a131e1ced118da2eb3c74251a79b8750df3e832
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-04-13 21:45:14 +0800

    Delete sample_v2.gz

--- process_data_tick.py
+++ process_data_tick.py
@@ -6,7 +6,6 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 parser.add_argument('--ticker', type=str, default='HSBA')
 args = parser.parse_args()
-
 ticker = args.ticker
 
 months = ['2016-01-01_2016-01-31',
@@ -19,32 +18,32 @@ months = ['2016-01-01_2016-01-31',
                     '2016-08-01_2016-08-31',
                     '2016-09-01_2016-09-30',
                     '2016-10-01_2016-10-31',
-                    '2016-11-01_2016-11-30']
-                    # '2017-01-01_2017-01-31',
-                    # '2017-02-01_2017-02-28',
-                    # '2017-03-01_2017-03-31',
-                    # '2017-04-01_2017-04-30',
-                    # '2017-05-01_2017-05-31',
-                    # '2017-06-01_2017-06-30',
-                    # '2017-07-01_2017-07-31',
-                    # '2017-08-01_2017-08-31',
-                    # '2017-09-01_2017-09-30',
-                    # '2017-10-01_2017-10-31',
-                    # '2017-11-01_2017-11-30',
-                    # '2018-01-01_2018-01-31',
-                    # '2018-02-01_2018-02-28',
-                    # '2018-03-01_2018-03-31',
-                    # '2018-04-01_2018-04-30',
-                    # '2018-05-01_2018-05-31',
-                    # '2018-06-01_2018-06-30',
-                    # '2018-07-01_2018-07-31',
-                    # '2018-08-01_2018-08-31',
-                    # '2018-09-01_2018-09-30',
-                    # '2018-10-01_2018-10-31',
-                    # '2018-11-01_2018-11-30']
+                    '2016-11-01_2016-11-30',
+                    '2017-01-01_2017-01-31',
+                    '2017-02-01_2017-02-28',
+                    '2017-03-01_2017-03-31',
+                    '2017-04-01_2017-04-30',
+                    '2017-05-01_2017-05-31',
+                    '2017-06-01_2017-06-30',
+                    '2017-07-01_2017-07-31',
+                    '2017-08-01_2017-08-31',
+                    '2017-09-01_2017-09-30',
+                    '2017-10-01_2017-10-31',
+                    '2017-11-01_2017-11-30',
+                    '2018-01-01_2018-01-31',
+                    '2018-02-01_2018-02-28',
+                    '2018-03-01_2018-03-31',
+                    '2018-04-01_2018-04-30',
+                    '2018-05-01_2018-05-31',
+                    '2018-06-01_2018-06-30',
+                    '2018-07-01_2018-07-31',
+                    '2018-08-01_2018-08-31',
+                    '2018-09-01_2018-09-30',
+                    '2018-10-01_2018-10-31',
+                    '2018-11-01_2018-11-30']
 
 for month in months:
-    bar = tqdm(range(7))
+    bar = tqdm(range(6))
     bar.set_description('Reading Data -- {}'.format(month))
     path_name = '/nfs/home/mingweim/lob/{}/L2_{}.L_{}.csv.gz'.format(ticker, ticker, month)
 
@@ -60,18 +59,12 @@ for month in months:
 
     bar.update(1)
     bar.set_description('Rounding to Time Integer -- {}'.format(month))
-
     data['Date-Time'] = pd.to_datetime(data['Date-Time'],
-                                       format='%Y-%m-%dT%H:%M:%S.%fZ').dt.round('{}s'.format(300))
+                                       format='%Y-%m-%dT%H:%M:%S.%fZ').dt.round('{}s'.format(1))
 
-    bar.update(1)
-    bar.set_description('Grouping By -- {}'.format(month))
-
-    data = data.groupby(['Date-Time']).first().reset_index()
 
     bar.update(1)
     bar.set_description('Deleting Weekends -- {}'.format(month))
-
     data['Day'] = data['Date-Time'].dt.dayofweek
     data = data.drop(data.loc[(data['Day'] == 5) | (data['Day'] == 6)].index)
     data = data.drop(data.loc[(data['Date-Time'] >= pd.to_datetime('2016/7/14'))
@@ -94,35 +87,19 @@ for month in months:
     bar.update(1)
     bar.set_description('Storing Data -- {}'.format(month))
 
-    df_train = open('/nfs/home/mingweim/rl_for_optimal_exec/'
-                    'trading_environment/data/{}/{}.txt'.format(ticker, month), 'wb')
-    pickle.dump(data, df_train)
-    df_train.close()
     date = pd.to_datetime(data['Date-Time'].dt.strftime('%Y/%m/%d'))
     unique_date = pd.unique(date)
     for day in unique_date:
-        for session in ['morning', 'afternoon']:
-            df_train = open('/nfs/home/mingweim/rl_for_optimal_exec/trading_environment'
-                            '/data/{}/{}_{}_{}.txt'.format(ticker, month, day, session), 'wb')
-            if session == 'morning':
-                session_data = data.loc[data['Date-Time'] >= day + pd.Timedelta('{}hours'.format(8))]
-                session_data.reset_index(drop=True, inplace=True)
-                session_data = session_data.iloc[:49]
-                ext = session_data.loc[48]
-                if ext['Hour'] != 12:
-                    print('Day: ', day)
-                    print(session_data)
-            else:
-                session_data = data.loc[data['Date-Time'] >= day + pd.Timedelta('{}hours'.format(11))]
-                session_data.reset_index(drop=True, inplace=True)
-                session_data = session_data.iloc[:49]
-                ext = session_data.loc[48]
-                if ext['Hour'] != 15:
-                    print('Day: ', day)
-                    print(session_data)
+        df_train = open('/nfs/home/mingweim/rl_for_optimal_exec/trading_environment'
+                        '/data/{}/{}_{}.txt'.format(ticker, month, day), 'wb')
+        session_data = data.loc[data['Date-Time'] >= day + pd.Timedelta('{}hours'.format(8))]
+        session_data = session_data.iloc[[2000 * i for i in range(30)],]
+        session_data.reset_index(drop=True, inplace=True)
+        ext = session_data.loc[29][0]
+        print(ext)
 
-            pickle.dump(session_data, df_train)
-            df_train.close()
+        pickle.dump(session_data, df_train)
+        df_train.close()
 
     bar.update(1)
     bar.set_description('Finished Processing Data -- {}'.format(month))
