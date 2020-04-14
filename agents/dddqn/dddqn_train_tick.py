2d98787d0696f5cf85d9ef779269524bccb238fe
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-04-14 21:27:30 +0800

    Update dddqn_train_tick.py

--- agents/dddqn/dddqn_train_tick.py
+++ agents/dddqn/dddqn_train_tick.py
@@ -55,9 +55,7 @@ def dddqn_train(hyperparameters, ac_dict, ob_dict, train_months, test_months):
         for month in train_months:
             train_dict[month] = {}
             with open(os.getcwd() + '/trading_environment/data/{}/{}.txt'.format(ticker, month), 'rb') as df_train:
-                data = pickle.load(df_train, encoding='iso-8859-1')
-            date = pd.to_datetime(data['Date-Time'].dt.strftime('%Y/%m/%d'))
-            unique_date = pd.unique(date)
+                unique_date = pickle.load(df_train, encoding='iso-8859-1')
             train_date[month] = unique_date
             for day in unique_date:
                 train_dict[month][day] = {}
@@ -76,9 +74,7 @@ def dddqn_train(hyperparameters, ac_dict, ob_dict, train_months, test_months):
         for month in test_months:
             test_dict[month] = {}
             with open(os.getcwd() + '/trading_environment/data/{}/{}.txt'.format(ticker, month), 'rb') as df_test:
-                data = pickle.load(df_test, encoding='iso-8859-1')
-            date = pd.to_datetime(data['Date-Time'].dt.strftime('%Y/%m/%d'))
-            unique_date = pd.unique(date)
+                unique_date = pickle.load(df_test, encoding='iso-8859-1')
             test_date[month] = unique_date
             for day in unique_date:
                 test_dict[month][day] = {}
