bf426cc2b6b5221b347934a37992610e067d0c68
GongqiLi <56348686+GongqiLi@users.noreply.github.com>
2020-04-15 01:40:53 +0800

    Update orderbook.py

--- trading_environment/orderbook.py
+++ trading_environment/orderbook.py
@@ -22,7 +22,7 @@ class OrderBook:
         for ask_order in initial_orders[1]:
             self.handleLimitOrder(ask_order)
         self.base_price = initial_orders[0][-1]['PRICE']
-        # self.handleLimitOrder({'TYPE': 0, 'ORDER_ID': -1, 'PRICE': self.base_price, 'SIZE': 1e8, 'BUY_SELL_FLAG': 'BUY'})
+        self.handleLimitOrder({'TYPE': 0, 'ORDER_ID': -1, 'PRICE': self.base_price, 'SIZE': 1e8, 'BUY_SELL_FLAG': 'BUY'})
 
     def update(self, historical_orders):
         self.bids = []
@@ -32,7 +32,8 @@ class OrderBook:
         for ask_order in historical_orders[1]:
             self.handleLimitOrder(ask_order)
         self.base_price = historical_orders[0][-1]['PRICE']
-        # self.handleLimitOrder({'TYPE': 0, 'ORDER_ID': -1, 'PRICE': self.base_price, 'SIZE': 1e8, 'BUY_SELL_FLAG': 'BUY'})
+        self.handleLimitOrder({'TYPE': 0, 'ORDER_ID': -1, 'PRICE': self.base_price, 'SIZE': 1e8, 'BUY_SELL_FLAG': 'BUY'})
+        print(self.bids)
 
     def handleLimitOrder(self, input_order):
         """
